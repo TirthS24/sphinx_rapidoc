e04c776477f85fb429cfc38aa06caac371387c9a
Tirth Shah <tirths@york.ie>
2025-03-25 00:22:16 +0530

    Updated package for Python 3.9+ and Sphinx 7.3.5+ compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx_rapidoc',
-    version='0.1.1',
+    version='0.1.2',
     description='Custom Sphinx extension for embedding RapiDoc in your documentation',
     author='Tirth Shah',
     packages=find_packages(),
@@ -10,7 +10,7 @@ setup(
     zip_safe=False,
     url="https://github.com/TirthS24/sphinx_rapidoc",
     install_requires=[
-        'sphinx>=8.1.3',
+        'sphinx>=7.2.5',
     ],
     package_data={
         'sphinx_rapidoc': [
@@ -23,7 +23,7 @@ setup(
             'sphinx_rapidoc = sphinx_rapidoc',
         ],
     },
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     classifiers=[
